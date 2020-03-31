d3850a1c1c57f2e9b369b540c82efa1822b94c1d
Rodrigo <Rodrigo@DESKTOP-F67OHJN>
2020-03-31 17:47:12 +0200

    Version 1. Eclipse

---
+++ HolaMundo/src/holaMundo/HolaMundo.java
@@ -0,0 +1,33 @@
+package holaMundo;
+
+import java.util.Scanner;
+
+
+public class HolaMundo {
+	/*
+	 * aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+	 * bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+	 */
+	public void prueba() {
+
+		int num = 0;
+
+		if (1 < num) num++;
+
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Introduce algo:");
+		String algo=in.nextLine();
+
+		System.out.println("\r\n" + "1. Cambiar el número de espacios de tabulación a 5\n"
+				+ "2. En Control Statement en el if-else vamos a seleccionar crear todos los espacios que aparecen\n"
+				+ "3. En Comments cambiaremos el valor por defecto para que sean 200 caracteres por línea\n"
+				+ "4. En Blank Lines, añadiremos dos nuevas líneas en blanco después de la declaración de los import\n"
+				+ "5. Guarda los cambios y aplica tu estilo\n");
+
+	}
+
+}
